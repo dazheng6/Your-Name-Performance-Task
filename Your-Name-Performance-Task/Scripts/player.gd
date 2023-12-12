7ebb699a57b6fe4463ea93788abd1f4cd576ca8a
dazheng6 <dazheng6@s.sfusd.edu>
2023-12-12 12:10:24 -0800

    Sound Effects Added

--- Your-Name-Performance-Task/Scripts/player.gd
+++ Your-Name-Performance-Task/Scripts/player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @export var Speed = 50
+@onready var bg_music = $"../BGMusic"
+@onready var enemy_hit = $"../EnemyHit"
 
 func _process(velocity):
 	velocity = Vector2.ZERO
@@ -21,3 +23,5 @@ func _process(velocity):
 
 func _on_enemy_body_entered(body):
 	hide()
+	$"../EnemyHit".play()
+	$"../BGMusic".stop()
